1e00fd46602b8f89b7944b5ad60c221497953b64
Michael Sun <92928043+Oops303@users.noreply.github.com>
2024-11-01 18:34:20 -0400

    Upload D-flip-flop code

---
+++ Lab6/dff_syn.srcs/sim_1/new/dff_syn_tb.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/15/2024 03:14:46 PM
+// Design Name: 
+// Module Name: dff_syn_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/15/2024 02:51:24 PM
+// Design Name: 
+// Module Name: dff_asyn_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dff_syn_tb;
+reg clk;
+reg d;
+reg rstn;
+wire q;
+
+dff_syn uut ( .d(d), .rstn(rstn), .clk(clk), .q(q));
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    d = 0;
+    rstn = 0;
+    
+    #10 rstn = 1;
+    #10 d = 1;
+    #10 d = 0;
+    #10 d = 1;
+    #10 rstn = 0;
+    #10 rstn = 1;
+    #20 $finish;
+end
+endmodule
+
